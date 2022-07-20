0c45f62562d77b8800ca104cd5b304d24eb7d0e2
Sorin Iordache <jordache_sor@yahoo.com>
2022-07-20 18:03:04 +0300

    Add custom greeting

--- GitBasics/GitBasics/Program.cs
+++ GitBasics/GitBasics/Program.cs
@@ -1,12 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("What is your name?");
 string? name = Console.ReadLine();
+Console.WriteLine("What is your preferred greeting?");
+string? greeting = Console.ReadLine();
 if (name != "")
 {
-    Console.WriteLine($"Hi, {name}!");
+    Console.WriteLine($"{greeting}, {name}!");
 }
 else
 {
-    Console.WriteLine("Hello, stranger!");
+    Console.WriteLine($"{greeting}, stranger!");
 }
 
